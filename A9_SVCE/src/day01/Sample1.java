d75513be8988281bd8fb7d98033a58419270e6d0
Hp <Hp@DESKTOP-MEHK842>
2022-12-06 18:50:22 +0530

    First commit on git

---
+++ A9_SVCE/src/day01/Sample1.java
@@ -0,0 +1,11 @@
+package day01;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello SVCE...");
+
+	}
+
+}
---
+++ A9_SVCE/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Hp
+ *
+ */
+module A9_SVCE {
+}
